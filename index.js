2a48f32728f5acf7c8bbd4f551ecd326e6610716
Tejasmagdum2309 <magdumtej1008@gmail.com>
2023-10-06 21:02:36 +0530

    new index file

--- index.js
+++ index.js
@@ -1,21 +1,71 @@
-import { Client, GatewayIntentBits } from 'discord.js';
-import dotEnv from "dotenv"
-// const dotEnv = require('dotenv');
-dotEnv.config();
-const client = new Client({ intents: [GatewayIntentBits.Guilds , GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent] });
+// Require the necessary discord.js classes
+import { Client,Events, GatewayIntentBits } from 'discord.js';
+// import {mongoose} from 'mongoose';
+import { storeMessageData } from './mongoDb.js'; // Import the storeMessageData function
 
-client.on('messageCreate',(message)=>{
-    if(message.author.bot) {
-        
-        return;}  
-    message.reply({
-        content : "Hello there we are working on our project"
-    })
-    console.log(message.content);
+
+import dotEnv from "dotenv"
+
+dotEnv.config();
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.on("ready", c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`,
+    
+ );
+
 });
 
-client.on("interactionCreate",(intraction)=>{
-        intraction.reply("its easy..")
-})
+// (async()=>{
+//     mongoose.set('strictQuery', false);
+//     await mongoose.connect('mongoURL'); // write mongo yrl here
+//     console.log("mono connected");
+
+//     client.on('messageCreate', message => {
+    
+//     if (message.author.bot) return; 
+
+//     const githubUrlRegex = /(https?:\/\/)?(www\.)?github\.com\/[a-zA-Z0-9-]+\/[a-zA-Z0-9-]+/;
+
+//     // Check if the message content matches the GitHub URL pattern
+//     if (githubUrlRegex.test(message.content)) {
+//         message.reply('you saved your streak ');
+//     }
+//     else{
+//         message.reply(`${message.author.displayName} please send the github url too`);
+//     }
+    
+//     // if (message.content === '!ping') {
+//     //     message.channel.send('Pong!');
+//     // }
+//     // else{message.reply(`hello there ${message.author.displayName}`  );}
+
+//     // console.log(`Received message: ${message}`);
+
+    
+// });
+// })()
+
+
+
+// client.on('messageCreate',message =>{
+//     console.log(message);
+// })
+
+
+
+
+// Log in to Discord with your client's token
+
+
+client.on('messageCreate', message => {
+    // Store message data in the database
+    
+    storeMessageData(message.author.id, message.content);
+});
 
 client.login(process.env.SECREAT_AUTH);
